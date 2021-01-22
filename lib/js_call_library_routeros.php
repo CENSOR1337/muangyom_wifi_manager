d3caec202cfa9cf7a411e62a84e1b61e05428a04
DESKTOP-VEUHUMN\MeteeLaptop <MeteeLaptop>
2021-01-22 11:47:11 +0700

    no message

--- lib/js_call_library_routeros.php
+++ lib/js_call_library_routeros.php
@@ -3,14 +3,14 @@ header("Content-Type: application/json");
 
 require(realpath(dirname(__FILE__) . '/library_routeros.php'));
 $api_lib = new routeros_api_library();
-
+$Autha
 
 // this is for security check
 if (false) {
     return  print json_encode("Auth needed");;
 }
 
-if (isset($_POST['action']) && method_exists($api_lib, $_POST['action'])) {
+if (isset($_POST['action']) && method_exists($api_lib, $_POST['action']) && $auth_token) {
 
     $action = $_POST['action'];
     $parameter = ($_POST['parameter'] != null) ? $_POST['parameter'] : null;
